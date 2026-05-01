667b0e50c6ece6feb0d335896dc25931eb200fcb
JM3L0 <jousarufinoleal@gmail.com>
2026-05-01 15:26:43 -0300

    feat: initialize regression script with salary dataset loading and data splitting imports

--- Trabalho 2/Trabalho_regressao.py
+++ Trabalho 2/Trabalho_regressao.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+salary = pd.read_csv('https://github.com/ybifoundation/Dataset/raw/main/Salary%20Data.csv')
+
+salary.columns
+
+y = salary['Salary']
+X = salary[['Years_Experience']]
+
+from sklearn.linear_model import train_test_split
